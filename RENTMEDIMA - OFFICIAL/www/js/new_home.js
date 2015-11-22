70d3625eb674f2d90a45035e0e78e8b8dc00e6c2
Stefano Tolomeo <Stefano Tolomeo>
2015-11-23 00:34:17 +0100

    XDDDD

---
+++ RENTMEDIMA - OFFICIAL/www/js/new_home.js
@@ -0,0 +1,137 @@
+$(document).ready(getInfo);
+
+function getInfo(){ 
+    console.log("home");
+    myUser= JSON.parse(localStorage.getItem("userData"));
+    console.log(myUser);
+    $("#uloginType").html(myUser.loginType);   
+    $("#uId").html(myUser.id);   
+    $("#uName").html(myUser.name);   
+    $("#uSurname").html(myUser.surname);   
+    $("#uMail").html(myUser.email);
+    $("#uToken").html(myUser.token);
+    $("#uImg").attr('src',myUser.picture);
+}
+
+function logout() {
+    console.log("logout");
+    myUser= JSON.parse(localStorage.getItem("userData"));
+    myUrl=  "http://rentme.altervista.org/logout.php?"       +
+                        "id="           +   myUser.id                     +                
+                        "&email="       +   myUser.email                  ;       
+    console.log(myUser);
+                xhttp = new XMLHttpRequest;
+                xhttp.open("GET", myUrl, false);
+                xhttp.send();                                
+    if(myUser.loginType=='Google'){
+        $.post('https://accounts.google.com/o/oauth2/revoke', {
+                    token: localStorage.access_token
+            })
+            .done(function(data) {
+                    console.log("token rimosso");
+                    //localStorage.removeItem('access_token');
+                    localStorage.clear();
+                    //var link="../index.html";
+                    var link="../index.html";            
+                    window.location.href=link;
+            })
+            .fail(function(response) {
+                    console.log("token error");
+            });
+    }else
+        if(myUser.loginType=='Facebook'){    
+            openFB.init({appId: '867006893383189', tokenStore: window.localStorage});
+            openFB.logout(
+                function() {
+                    console.log("Logout Successfull");
+                    window.location.href="../index.html";
+                });
+        }else
+             if(myUser.loginType=='rentMe'){    
+                 localStorage.clear();
+                 window.location.href="../index.html";
+             }
+        
+} 
+
+/*function getStatus(){
+                openFB.api({
+                    path: '/me',
+                    success: function(data) {
+                       // document.getElementById("login").hidden=true;
+                         document.getElementById("infos").hidden=false;
+                         document.getElementById("logout").hidden=false;
+                         document.getElementById("revoke").hidden=false;
+                    },
+                    error: function(data) {
+                       // document.getElementById("login").hidden=false;
+                         document.getElementById("infos").hidden=true;
+                         document.getElementById("logout").hidden=true;
+                         document.getElementById("revoke").hidden=true;
+                    }
+                });
+            }
+            function login() {
+                openFB.login(
+                    function(response) {
+                        if(response.status === 'connected') {
+                            window.location.href="home.html";
+                            //alert('Facebook login succeeded, got access token: ' + response.authResponse.accessToken);
+                           // openFB.tokenStore =  response.authResponse;
+                            //console.log(openFB.tokenStore.fbAccessToken);
+                        } else {
+                            alert('Facebook login failed: ' + response.error);
+                        }
+                    }, {scope: 'email'});
+            }
+            function getInfo() {
+                openFB.api({
+                    path: '/me',
+                    success: function(data) {
+                        console.log("DATI:" + JSON.stringify(data));
+                        document.getElementById("userName").innerHTML = data.first_name +" " + data.last_name;
+                        document.getElementById("email").innerHTML = data.email;
+                        //document.getElementById("userPic").src = 'http://graph.facebook.com/' + data.id + '/picture?type=small';
+                    },
+                    error: errorHandler});
+            }
+            function share() {
+                openFB.api({
+                    method: 'POST',
+                    path: '/me/feed',
+                    params: {
+                        message: document.getElementById('Message').value || 'Testing Facebook APIs'
+                    },
+                    success: function() {
+                        alert('the item was posted on Facebook');
+                    },
+                    error: errorHandler});
+            }
+            function readPermissions() {
+                openFB.api({
+                    method: 'GET',
+                    path: '/me/permissions',
+                    success: function(result) {
+                        alert(JSON.stringify(result.data));
+                    },
+                    error: errorHandler
+                });
+            }
+            function revoke() {
+                openFB.revokePermissions(
+                        function() {
+                            console.log("Permission Revoked");
+                             window.location.href="index.html"
+                        },
+                        errorHandler);
+            }
+            function logout() {
+                openFB.logout(
+                        function() {console.log("Logout Successfull");
+                            window.location.href="index.html"
+                        },
+                        errorHandler);
+            }
+            function errorHandler(error) {
+                console.log(error.message);
+    }*/
---
+++ RENTMEDIMA - OFFICIAL/www/js/startPage.js
@@ -0,0 +1,159 @@
+$(function() {
+    console.log("START PAGE");
+    openFB.init({appId: '867006893383189', tokenStore: window.localStorage}); 
+    
+    $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+});
+
+function facebookLogin() {
+    openFB.login(
+        function(response) {
+            if(response.status === 'connected') {               
+                openFB.api({
+                    path: '/me',
+                    success: function(user) {                                    
+                        myUrl=  "http://rentme.altervista.org/login.php?"       +
+                                "id="           +   user.id                     +
+                                "&name="        +   user.first_name             +
+                                "&surname="     +   user.last_name              +
+                                "&email="       +   user.email                  +
+                                "&loginType="   +   'Facebook'                  +
+                                "&token="       +   localStorage.fbAccessToken  +
+                                "&picture="     +   String(user.picture.data.url).replace(/&/gi,'%26')      ; 
+                        
+                        xhttp = new XMLHttpRequest;
+                        xhttp.open("GET", myUrl, false);
+                        xhttp.send();                    
+                        jUser=xhttp.response;  
+                        if(JSON.parse(jUser).id!=null){
+                            console.log("dentro");
+                            localStorage.setItem("userData",jUser);
+                            setTimeout(function(){
+                                        window.location.href="new_home.html";
+                            },50);                          
+                        }else{                                   
+                            navigator.notification.alert(JSON.parse(jUser).message, reload, JSON.parse(jUser).title);      
+                        }
+                    },
+                    error:function(result){  
+                        console.log(result);
+                    }                                
+                });
+            } else {
+                //alert('Facebook login failed: ' + response.error);
+            }
+        }, {scope: 'email'});
+                        
+}
+
+function googleLogin(){    
+        googleapi.authorize({client_id: app_google.client_id,
+            client_secret: app_google.client_secret,
+            redirect_uri: app_google.redirect_uri,
+            scope: app_google.scope
+        }).done(function() {           
+            googleapi.getToken({
+                client_id: app_google.client_id,
+                client_secret: app_google.client_secret
+            }).then(function(data) {
+                //Pass the token to the API call and return a new promise object
+                return googleapi.userInfo({ access_token: data.access_token });
+            }).done(function(user) {                                   
+                myUrl=  "http://rentme.altervista.org/login.php?"       +
+                        "id="           +   user.id                     +
+                        "&name="        +   user.given_name             +
+                        "&surname="     +   user.family_name            +
+                        "&email="       +   user.email                  +
+                        "&loginType="   +   'Google'                    +
+                        "&token="       +   localStorage.access_token   +
+                        "&picture="     +   String(user.picture).replace(/&/gi,'%26')                      ;                        
+                xhttp = new XMLHttpRequest;
+                xhttp.open("GET", myUrl, false);
+                xhttp.send();
+                jUser=xhttp.response;
+                if(JSON.parse(jUser).id!=null){
+                    console.log("dentro");
+                    localStorage.setItem("userData",jUser);
+                    setTimeout(function(){
+                                window.location.href="new_home.html";
+                    },50);                          
+                }else{         
+                    navigator.notification.alert(JSON.parse(jUser).message, reload, JSON.parse(jUser).title);       
+                }                
+            }).fail(function() {
+                //handle error
+            });
+        }).fail(function(data) {
+            //Show an error message if access was denied            
+        });   
+}
+
+// Funziona, a parte il messaggio di errore che si intravede
+function login(){
+    //console.log("Dentro login function");
+  myUrl=  "http://rentme.altervista.org/login.php?" +                
+            "email="       +   document.getElementById('emailLogin').value      +
+            "&loginType="   +   'rentMe'                                    +
+            "&password="    +   document.getElementById('passwordLogin').value   ;                        
+    xhttp = new XMLHttpRequest;
+    xhttp.open("GET", myUrl, false);
+    xhttp.send();
+    jUser=xhttp.response;
+    if(JSON.parse(jUser).id!=null){
+        console.log("dentro");
+        localStorage.setItem("userData",jUser);
+        setTimeout(function(){
+                    window.location.href="new_home.html";
+        },50);                          
+    }else{                   
+        navigator.notification.alert(JSON.parse(jUser).message, reload, JSON.parse(jUser).title);    
+    }
+}
+
+function register(){   
+    console.log("dentro register");
+    myUrl=  "http://rentme.altervista.org/registration.php?" +                
+            "name="        +   document.getElementById('nameReg').value       +
+            "&surname="     +   document.getElementById('surnameReg').value    +
+            "&email="       +   document.getElementById('emailReg').value      +
+            "&loginType="   +   'rentMe'                                    +
+            "&password="    +   document.getElementById('passwordReg').value   ;                       
+    console.log("ciaaaaaooo!!11!!");
+
+    xhttp = new XMLHttpRequest;
+    xhttp.open("GET", myUrl, false);
+    xhttp.send();
+    jUser=xhttp.response;
+    if(JSON.parse(jUser).id!=null){        
+        localStorage.setItem("userData",jUser);  
+        setTimeout(function(){
+                    window.location.href="new_home.html";            
+        },50);    
+    }else{     
+        console.log("ERROR");
+        console.log(jUser);  
+        navigator.notification.alert(JSON.parse(jUser).message, reload, JSON.parse(jUser).title);    
+    }
+    
+}
+
+function reload(){
+    console.log("RELOAD");
+     setTimeout(function(){         
+            window.location.reload(true);
+        },50);
+}
