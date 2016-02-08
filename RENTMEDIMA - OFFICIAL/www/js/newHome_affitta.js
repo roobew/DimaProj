3e5ff1df8ca10ec1037301f6c2d8c7c1fe3bb429
Stefano Tolomeo <Stefano Tolomeo>
2016-02-08 11:13:05 +0100

    zxsdc

--- RENTMEDIMA - OFFICIAL/www/js/newHome_affitta.js
+++ RENTMEDIMA - OFFICIAL/www/js/newHome_affitta.js
@@ -1,16 +1,24 @@
-var replaceWith = $('<input name="temp" type="text" />'),
+var replaceWith = $('<input type="text" style="width:100%;" placeholder="Nome annuncio" />'),
     connectWith = $('input[name="hiddenField"]');
 var firstModify=true;
 
+var divToAdd="<div class='annuncioDiv'><div class='annuncioDelDiv'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></div><div class='annuncioContentDiv'></div></div>"
+
+var countDeleteClick=0;
+
+var newAnnuncio;
+var titoloNuovoAnnuncio;
+
 $(document).ready(function (){
-    $("#nuovoAnnuncioDiv").hide();
-    
-    $("#btnNuovoAnnuncio").button();
-    
+
+    $("#affittaContent_creaAnnuncio").hide();
+        
     $("#btnNuovoAnnuncio").click(function (){
-        $(this).after("<form ><input type='text' class='annuncioDiv'> </input></form>");  
+        $("#buttonsDiv").after(divToAdd);  
+        newAnnuncio=$('.annuncioDiv').first();
+        editText(newAnnuncio);
         //$('.annuncioDiv').first().inlineEdit(replaceWith, connectWith);
-        $('.annuncioDiv').first().focus();
+        //$('.annuncioDiv').first().focus();
     });
     
     $('#provaFocus').bind("keydown", function(e){
@@ -25,26 +33,104 @@ $(document).ready(function (){
     $("#provaFocus").focusout(function(){
         console.log("focused OUT!!");   
     });
-    /*$("#provaFocus").change(function(){
-        if(firstModify==true){
-            $(this).d
+   
+    $("#btnDeleteAnnuncio").click(function (){
+        
+        $(".annuncioContentDiv").css("display","inline-block");
+        if(countDeleteClick%2==0){    
+            $(this).html("Fine");
+            openDeleteElement();
+        }
+        else{
+            $(this).html("Elimina"); 
+            closeDeleteElement();
+        }
+        countDeleteClick++;
+    });
+    
+    $("#annullaButton").click(function (){
+        var res=confirm("Annullando l'annuncio verrà eliminato. Sei sicuro di voler procedere?");   
+        if(res==true){
+            rimuoviAnnuncio(newAnnuncio,false);
+
+            $("#affittaContent_creaAnnuncio").fadeOut();
+            $("#affittaContent").fadeIn();
         }
     });
-    */
 });
 
-$.fn.inlineEdit = function(replaceWith, connectWith) {
+jQuery(document).on('click','.annuncioDelDiv', function(event) { 
+       // alert("cliccato "+ $(this).siblings().text());
+        var res=confirm("Eliminare l'annuncio '"+$(this).siblings().text()+"' ? ");   
+        
+        if(res==true){
+            rimuoviAnnuncio($(this).parent(), true);      
+        }
+    });
 
-    $(this).hover(function() {
+//Riceve in ingresso il div ".annuncioDiv" e un bool per l'animazione
+function rimuoviAnnuncio(elementToRemove, animation){
+    if(animation==true){   
+        /*elementToRemove.animate({
+            width: '0'   
+        }, 800); 
+
+        elementToRemove.animate({
+            width: '0'   
+        }, 800, function(){
+            elementToRemove.remove();    
+        });  */
+        
+        elementToRemove.fadeOut('fast', 'easeOutCubic', function(){
+            elementToRemove.remove();   
+        });
+    }
+    else{
+        elementToRemove.remove();   
+    }
+}
+
+function openDeleteElement(){
+    
+    $(".annuncioContentDiv").animate({
+        left: '40px',
+        width: '95%'   
+    }, 300);
+    
+    
+    
+    $(".annuncioDelDiv").animate({
+        width: '40px'
+    }, 300);
+    $(".annuncioDelDiv").fadeIn(800).css("display","inline-block"); 
+}
+
+function closeDeleteElement(){
+    $(".annuncioContentDiv").animate({
+        left: '0px',
+        width: '100%'   
+    }, 400);
+    
+    
+    
+    $(".annuncioDelDiv").animate({
+        width: '0px'
+    }, 400, function(){
+        $(".annuncioDelDiv").fadeOut(800).css("display","none");   
+    });
+     
+}
+
+function editText(nuovoAnnuncioDiv) {
+   /* $(this).hover(function() {
         $(this).addClass('hover');
     }, function() {
         $(this).removeClass('hover');
-    });
-
-    $(this).click(function() {
-
-        var elem = $(this);
-
+    });*/
+    
+        var elem = nuovoAnnuncioDiv.find(".annuncioContentDiv");
+        
+        console.log(elem);
         elem.hide();
         elem.after(replaceWith);
         replaceWith.focus();
@@ -54,6 +140,57 @@ $.fn.inlineEdit = function(replaceWith, connectWith) {
             if ($(this).val() != "") {
                 connectWith.val($(this).val()).change();
                 elem.text($(this).val());
+                $(this).val("");
+            }
+            else{
+               elem.text("Nuovo annuncio");   
+            }
+            titoloNuovoAnnuncio=elem.text();
+            console.log("l'annuncio è: "+titoloNuovoAnnuncio);
+            $(this).remove();
+            elem.show();
+            
+            creaAnnuncio();
+            
+        });
+
+}
+
+function creaAnnuncio(){
+    $("#affittaContent").fadeOut();
+    $("#affittaContent_creaAnnuncio").fadeIn();
+    
+    $("#titoloAnnuncio").text(titoloNuovoAnnuncio);
+    
+    
+}
+
+/*
+$.fn.inlineEdit = function(replaceWith, connectWith) {
+    
+    $(this).hover(function() {
+        $(this).addClass('hover');
+    }, function() {
+        $(this).removeClass('hover');
+    });
+
+    $(this).click(function() {
+        
+        var elem = $(this);
+        //console.log(elem);
+        
+        elem.hide();
+        elem.after(replaceWith);
+        replaceWith.focus();
+        //console.log(replaceWith);
+        
+        replaceWith.blur(function() {
+
+            console.log("BLUR event");
+            if ($(this).val() != "") {
+                connectWith.val($(this).val()).change();
+                elem.text($(this).val());
+                replaceWith.val("");
             }
 
             $(this).remove();
@@ -61,3 +198,4 @@ $.fn.inlineEdit = function(replaceWith, connectWith) {
         });
     });
 };
+*/
