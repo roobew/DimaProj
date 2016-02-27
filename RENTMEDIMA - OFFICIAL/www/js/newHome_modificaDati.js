d43d704e3f2524ec3739ace8f6c0ba8ccde7f0dc
Stefano Tolomeo <Stefano Tolomeo>
2016-02-27 19:42:07 +0100

    qwert

--- RENTMEDIMA - OFFICIAL/www/js/newHome_modificaDati.js
+++ RENTMEDIMA - OFFICIAL/www/js/newHome_modificaDati.js
@@ -1,57 +1,146 @@
+//var newValue, valueToShow;
 
 $(document).ready(startModificaDati);
 
 function startModificaDati(){
-    var newValue;
-    var inputElement;
-    var elementToOverride;
+   /*
+    var lastValue, inputElement, textElement, elementToOverride;
+
+    $(".modificaIconText").on("click", function(){
+        
+        inputElement = $(this).siblings(".userDataInputText");
     
-    $(".modificaIconText").on("tap", function(){
+        textElement = $(this).siblings(".userDataText").hide();
         
-        inputElement= $(this).siblings(".userDataInputText");
-        
-        $(this).siblings(".userDataText").hide();
+        if(inputElement.hasClass("passwordClass")==true){
+            lastValue = "";
+            console.log("VERO");
+        }
+        else{
+            lastValue = textElement.text();
+            console.log("FALSO");
+        }
+            
+        inputElement.val(lastValue);
+        console.log("Last value è: "+lastValue);
+
+        textElement.hide();
         inputElement.show();
-        
+
         inputElement.focus();
-                
+        
     });
     
-    $(".userDataInputText").blur(function() {
-        newValue=$(this).val();
-        console.log(newValue);
+    $(".userDataInputText").blur(function (){
+        console.log("Blur EVENT");
+        // InputElement è $(this)
         
-        elementToOverride= $(this).siblings(".userDataText");
+        textElement = $(this).siblings(".userDataText");
         
-        if(elementToOverride.attr("id") == "pwInputField"){
-            console.log("hiiiii");
-            // show confirm password input text
-            $(this).parent().next().find("#confermaPwUtenteInput")
+        if($(this).hasClass("passwordClass")==true){
+            valueToShow = "**********";
+           
         }
         else{
-            elementToOverride.text(newValue);       
+            valueToShow = $(this).val();
         }
         
+        if($(this).attr("id")=="passwordUtenteInput"){
+            $("#confermaPasswordModificaIcon").show();   
+        }
         
+        newValue = $(this).val();
+        console.log("Input Value è: "+newValue);
+        
+        textElement.text(valueToShow);
         $(this).hide();
-        $(this).siblings(".userDataText").show();
-        /*
-        if ($(this).val() != "") {
-            connectWith.val($(this).val()).change();
-            elem.text($(this).val());
-            $(this).val("");
+        textElement.show();
+        
+        
+    });
+    */
+    
+    var iconClicked;
+    var inputElement;
+    var textElement;
+    var iconCheckedElement;
+    var blurElementID;
+    
+    $(".modificaIconText").on("click", function(){
+        iconClicked=$(this).attr("id");
+        console.log("Icon clicked è: "+iconClicked);
+        
+        if(iconClicked=="modificaDatiNomeIcon"){
+            inputElement = $("#modificaDatiNomeInput");
+            textElement = $("#modificaDatiNomeText");
+            iconCheckedElement = $("#modificaDatiNomeCheckedIcon");
         }
-        else{
-           elem.text("Nuovo annuncio");   
-        }
-        titoloNuovoAnnuncio=elem.text();
-        console.log("l'annuncio è: "+titoloNuovoAnnuncio);
-        $(this).remove();
-        elem.show();
-
-        */
-
+        
+        showInputText(inputElement, textElement, iconCheckedElement);
+ 
     });
     
+    $(".userDataInputText").blur(function (){
+        blurElementID=$(this).attr("id");
+        console.log("Blur Element ID è: "+blurElementID);
+        
+        inputElement = $(this);
+        if(blurElementID=="modificaDatiNomeInput"){
+            
+            textElement = $("#modificaDatiNomeText");
+            iconCheckedElement = $("#modificaDatiNomeCheckedIcon");
+        }  
+        
+        showTextField(inputElement, textElement, iconCheckedElement);
+        
+    });
     
+}
+
+function showInputText(myInput, myText, myIcon){
+        var lastValue;
+    
+        myText.hide();
+    
+        if(myInput.hasClass("passwordClass")==true){
+            lastValue = "";
+            console.log("VERO");
+        }
+        else{
+            lastValue = myText.text();
+            console.log("FALSO");
+        }
+            
+        myInput.val(lastValue);
+        console.log("Last value è: "+lastValue);
+
+        myText.hide();
+        myInput.show();
+
+        myInput.focus();
+        
+}
+    
+function showTextField(myInput, myText, myIcon){
+    
+    var valueToShow, newValue;
+    
+    if(myInput.hasClass("passwordClass")==true){
+        valueToShow = "**********";
+
+    }
+    else{
+        valueToShow = myInput.val();
+    }
+
+    if(myInput.attr("id")=="passwordUtenteInput"){
+        $("#confermaPasswordModificaIcon").show();   
+    }
+
+    newValue = myInput.val();
+    console.log("Input Value è: "+newValue);
+
+    myText.text(valueToShow);
+    myInput.hide();
+    myText.show();
 }
